371375a188661e15f29d8828063a114e369f804d
vaishnavi sangolli <iam.vaishnavisangolli@gmail.com>
2026-01-19 14:18:14 +0530

    save

---
+++ test_power.py
@@ -0,0 +1,8 @@
+from power import power
+   def power test_1():
+       assert power_test_1==4
+   def power():
+       assert power_test_1(2,3)==6
+   def power test_3():
+       assert power_test_1==8
+ 
